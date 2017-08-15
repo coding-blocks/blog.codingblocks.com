3b5d0726a69e0f9a67f6410ccbdc96669d2237d8
Arnav Gupta <arnav@codingblocks.com>
2017-08-16 01:24:18 +0530

    update katex init
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- js/katex_init.js
+++ js/katex_init.js
@@ -2,21 +2,25 @@ var elements = document.getElementsByTagName('script')
 
 Array.prototype.forEach.call(elements, function(element) {
   if (element.type.indexOf('math/tex') != -1) {
-     // Extract math markdown
-     var textToRender = element.innerText || element.textContent;
+    // Extract math markdown
+    var textToRender = element.innerText || element.textContent;
 
-     // Create span for KaTeX
-     var katexElement = document.createElement('span');
+    // Kramdown and KaTeX workaround
+    // https://github.com/rohanchandra/type-theme/issues/47
+    textToRender = textToRender.replace(/%.*/g, '');
 
-     // Support inline and display math
-     if (element.type.indexOf('mode=display') != -1){
-       katexElement.className += "math-display";
-       textToRender = '\\displaystyle {' + textToRender + '}';
-     } else {
-       katexElement.className += "math-inline";
-     }
+    // Create span for KaTeX
+    var katexElement = document.createElement('span');
 
-     katex.render(textToRender, katexElement);
-     element.parentNode.insertBefore(katexElement, element);
+    // Support inline and display math
+    if (element.type.indexOf('mode=display') != -1){
+      katexElement.className += "math-display";
+      textToRender = '\\displaystyle {' + textToRender + '}';
+    } else {
+      katexElement.className += "math-inline";
+    }
+
+    katex.render(textToRender, katexElement);
+    element.parentNode.insertBefore(katexElement, element);
   }
 });
